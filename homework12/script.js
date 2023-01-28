522430c46b0d621ca951a115e7e712a9d889dfa6
asasin098676 <asasin09867@gmail.com>
2023-01-28 14:37:42 +0200

    hm12 new try

--- homework12/script.js
+++ homework12/script.js
@@ -1,58 +1,13 @@
-const url = "https://swapi.dev/api";
+const url = "https://swapi.dev/api/films/";
 const mainDiv = document.getElementById("mainDiv");
 const div = document.getElementById("secondDiv");
 const button = document.getElementById("startButton");
+const filmValue = document.getElementById("filmNumber");
 const mainPlanetDiv = document.getElementById("planetSlider");
-const moviePosters = {
-  1: "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
-  2: "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_.jpg",
-  3: "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_.jpg",
-  4: "https://static.posters.cz/image/750/%D0%9F%D0%BB%D0%B0%D0%BA%D0%B0%D1%82%D0%B8/star-wars-a-new-hope-one-sheet-i28733.jpg",
-  5: "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
-  6: "https://cloud.vistaapps.elcapitantheatre.com/CDN/media/entity/get/FilmPosterGraphic/h-HO00000182",
-};
-getPlanets(1);
 
-async function getValueOfEpisode() {
+async function getValueOfSomeEpisode() {
   div.innerHTML = "";
-
-  const results = (await (await fetch(`${url}/films`)).json()).results;
-
-  for (let i = 0; i < results.length; i++) {
-    const { release_date, title, episode_id } = results[i];
-
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "newDiv");
-    div.appendChild(newDiv);
-
-    newDiv.innerHTML = `
-     <h2>Ephisode${episode_id}:  ${title}</h2>
-     <img src = ${moviePosters[episode_id]}>
-     <h2>date of release: ${release_date}</h2>
-     <button class="infoBut" onclick="getValueOfSomeEpisode(${episode_id})" >get Information</button>
-    `;
-  }
-  getPlanets();
-}
-
-async function getValueOfSomeEpisode(per) {
-  div.innerHTML = `
-        <button
-          class="backButton"
-          onclick="getValueOfEpisode()"
-        >
-          back
-        </button>
-    `;
-
-  let newRes = 0;
-  if (per == 6 || per == 5 || per == 4) {
-    newRes = per - 3;
-  } else {
-    newRes = per + 3;
-  }
-
-  const res = await (await fetch(`${url}/films/${newRes}`)).json();
+  const res = await (await fetch(`${url}${filmValue.value}`)).json();
   for (let i = 0; i < res.characters.length; i++) {
     const person = await (await fetch(res.characters[i])).json();
     const newDiv = document.createElement("div");
@@ -89,23 +44,22 @@ async function getImgPerson(name) {
 }
 
 async function getPlanets(val1) {
-  const planets = (await (await fetch(`${url}/planets/?page=${val1}`)).json())
-    .results;
+  const planets = (
+    await (await fetch(`https://swapi.dev/api/planets/?page=${val1}`)).json()
+  ).results;
   for (let i = 0; i < planets.length; i++) {
-    console.log(planets[i].name);
     const newUl = document.createElement("ul");
     mainPlanetDiv.appendChild(newUl);
     newUl.innerHTML = `<h2>${planets[i].name}</h2>`;
   }
 }
 let count1 = 2;
-function rite() {
+function next() {
   count1++;
   mainPlanetDiv.innerHTML = "";
 
   if (count1 > 6) {
     count1 = 1;
   }
-  console.log(count1);
   getPlanets(count1);
 }
