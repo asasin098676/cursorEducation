f18c0aa279aab0b2d773a4bbdb9ced4c54d08785
asasin098676 <asasin09867@gmail.com>
2023-02-24 20:58:05 +0200

    hm21

--- homework20/navigation/src/navigationComp/NavigationComp.js
+++ homework20/navigation/src/navigationComp/NavigationComp.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./navigationComp.css";
+import { Link } from "react-router-dom";
 
 const NavigationComp = () => {
   return (
-    <div className="mainDiv">
-      <button>головна</button>
-      <button>публікації</button>
-      <button>фотографії</button>
-      <button>контакти</button>
-    </div>
+    <header>
+      <Link to="main">головна</Link>
+      <Link to="publication">публікації</Link>
+      <Link to="photo">фотографії</Link>
+      <Link to="contact">контакти</Link>
+    </header>
   );
 };
 
